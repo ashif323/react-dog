ddc68ccac08f5eb133152e0ca00e102c7443f53e
ashif323 <ashifiqbal0783@gmail.com>
2026-02-14 10:28:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+import Dog from './components/dog'
+import { Canvas } from '@react-three/fiber'
+
+function App() {
+  
+
+  return (
+    <main>
+      <Canvas style={{
+        height:"100vh",
+        width:"100vw",
+        position:"fixed",
+        top:0,
+        left:0,
+        zIndex:1,
+        backgroundImage:"url(models/background-l.png)",
+        backgroundRepeat:"no-repeat",
+        backgroundSize:"cover"
+      }}>
+        <Dog/>
+      </Canvas>
+      <section id='section-1'></section>
+      <section id='section-2'></section>
+      <section id='section-3'></section>
+    </main>
+    
+    
+  )
+}
+
+export default App
